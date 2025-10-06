580f9ad0e21c85f50af1e927e60239fac7a46a43
CleoKiama <cleophaskiama7@gmail.com>
2025-10-06 15:30:22 +0300

    chore: better error handling for the getserver session

--- utils/session.server.ts
+++ utils/session.server.ts
@@ -2,12 +2,21 @@
 import { auth } from "@/utils/auth.server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { tryCatch } from "./misc";
 
 export async function getServerSession() {
-  const session = await auth.api.getSession({
+  const sessionPromise = auth.api.getSession({
     headers: await headers(),
   });
-  return session ?? null;
+
+  const { data: session, error } = await tryCatch(sessionPromise);
+
+  if (error) {
+    console.error("Error getting the user session");
+    return null;
+  }
+
+  return session;
 }
 
 export async function requireAuth() {
